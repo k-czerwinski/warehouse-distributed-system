02a4522e5900371ce834781e18ddd39a24fd1bd9
Krzysztof Czerwiński <krzys.czerwinski@gmail.com>
2024-04-14 12:26:25 +0200

    Created spring boot project

---
+++ system/src/main/java/com/bd2/warehousedistributedsystem/WarehouseDistributedSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bd2.warehousedistributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseDistributedSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseDistributedSystemApplication.class, args);
+	}
+
+}
---
+++ system/src/test/java/com/bd2/warehousedistributedsystem/WarehouseDistributedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd2.warehousedistributedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseDistributedSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
